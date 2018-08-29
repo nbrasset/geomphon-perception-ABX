39952368e52e248c4864534f1943356cddf843a8
Amelia <post-doc@ogdoctd214.ogdoct.comp.univ-paris-diderot.fr>
2018-08-29 13:15:46 +0200

    bugfix in exit of intervals praat script

--- experiments/pilot_Aug_2018/geomphon_Aug_2018.sh
+++ experiments/pilot_Aug_2018/geomphon_Aug_2018.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+#navigate to correct folder 
+cd /Users/post-doc/Documents/GitHub/geomphon-perception-ABX/experiments/pilot_Aug_2018/
 
-for f in *.wav; do ffmpeg -i "$f" -c:a libmp3lame -q:a 2 "${f/%wav/mp3}" -c:a libvorbis -q:a 4 "${f/%wav/ogg}"; done
+#now run interval saving script
+/Applications/Praat.app/Contents/MacOS/Praat --open "save_intervals_to_wavs.Praat"
+/Applications/Praat.app/Contents/MacOS/Praat --run "save_intervals_to_wavs.Praat"
+
+
+
+
+# 
+#for f in *.wav; do ffmpeg -i "$f" -c:a libmp3lame -q:a 2 "${f/%wav/mp3}" -c:a libvorbis -q:a 4 "${f/%wav/ogg}"; done
 
 #
 
 
 
-for i in `ls *.aiff`
-do echo -e "$i"
- sox $i $i.wav echo -e "$i.wav"; done;
-for i in *.wav
-do
-    sox "$i" "stimuli/$(basename -s .mp3 "$i").mp3"
-done
+#for i in `ls *.aiff`
+#do echo -e "$i"
+# sox $i $i.wav echo -e "$i.wav"; done;
+#for i in *.wav
+#do
+#    sox "$i" "stimuli/$(basename -s .mp3 "$i").mp3"
+#done
