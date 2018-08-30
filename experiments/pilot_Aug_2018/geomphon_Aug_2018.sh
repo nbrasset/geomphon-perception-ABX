114ff00b5b1e132a52b045901eb9d28593682eb2
Amelia <post-doc@ogdoctd214.ogdoct.comp.univ-paris-diderot.fr>
2018-08-30 11:56:35 +0200

    adds normalisation script, updates intervals script to feed into normalization

--- experiments/pilot_Aug_2018/geomphon_Aug_2018.sh
+++ experiments/pilot_Aug_2018/geomphon_Aug_2018.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
-#navigate to correct folder 
-cd /Users/post-doc/Documents/GitHub/geomphon-perception-ABX/experiments/pilot_Aug_2018/
 
-#now run interval saving script
-/Applications/Praat.app/Contents/MacOS/Praat --open "save_intervals_to_wavs.Praat"
+#run interval saving script
 /Applications/Praat.app/Contents/MacOS/Praat --run "save_intervals_to_wavs.Praat"
 
+#run intensity normalization on all files that were just created 
+
+
+#run filtering script to get right stim list 
+
+
+#use stimlist to run concatenation file on normalized stimuli 
 
 
 
-# 
-#for f in *.wav; do ffmpeg -i "$f" -c:a libmp3lame -q:a 2 "${f/%wav/mp3}" -c:a libvorbis -q:a 4 "${f/%wav/ogg}"; done
-
-#
 
 
 
-#for i in `ls *.aiff`
-#do echo -e "$i"
-# sox $i $i.wav echo -e "$i.wav"; done;
-#for i in *.wav
-#do
-#    sox "$i" "stimuli/$(basename -s .mp3 "$i").mp3"
-#done
+
+
+#convert all new triplet files to .mp3 and .ogg and then save them to the LMEDs audio & video folder
+#	for f in *.wav; do ffmpeg -i "$f" -c:a libmp3lame -q:a 2 "${f/%wav/mp3}" -c:a libvorbis -q:a 4 "${f/%wav/ogg}"; done
+	#for i in `ls *.aiff`
+	#do echo -e "$i"
+	# sox $i $i.wav echo -e "$i.wav"; done;
+
+	#for i in *.wav
+	#do
+	#    sox "$i" "stimuli/$(basename -s .mp3 "$i").mp3"
+	#done
+
+
+
+
+
+
+
