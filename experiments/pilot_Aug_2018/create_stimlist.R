5616338a358fff7bfc7f66283ae6258f966c1668
Amelia <post-doc@ogdoctd214.ogdoct.comp.univ-paris-diderot.fr>
2018-08-30 16:32:50 +0200

    updates .mp3.ogg conversion to use Sox

--- experiments/pilot_Aug_2018/create_stimlist.R
+++ experiments/pilot_Aug_2018/create_stimlist.R
@@ -2,20 +2,26 @@
 #Aug 29 2018 by Amelia 
 # this script takes as input a .csv will all possible triplet combinations. 
 # the .csv is an output of ascript by Ewan named ______
-#the triplets are constrained as being the same 
+#the triplets are constrained as vowels compared only to vowels, consonants only to consonants
 #It then filters them to create a balanced subset, and generates a stimlist for LMEDs 
 # as well as a stimlist for the concatenation script. 
 
+#there are 72 different 
 library(dplyr)
 trips<-read.csv("/Users/post-doc/Documents/GitHub/geomphon-perception-ABX/experiments/pilot_Aug_2018/triplets.csv")
 
-#select only files that compare consonants to consonants 
-#and vowels to vowels 
-comps<-filter(trips, (CV_TGT=="C"&CV_OTH=="C"&CV_X=="C")|(CV_TGT=="V"&CV_OTH=="V"&CV_X=="V"))
+#do this with dplyr, n00b
+trips$trip_phons <- paste(trips$phone_TGT,trips$phone_OTH,trips$phone_X)
+trips$trip_phons<-as.factor(trips$trip_phons)
 
 
 
-#only files where Target and other are not the same phoneme (already controlled for) 
+#group by trip_phons
+#then group by context (/i/ vs /a/)
+#then group by TGT_speaker (E vs. A)
 
-summary(comps)
-#add index number 
+
+
+
+
+
