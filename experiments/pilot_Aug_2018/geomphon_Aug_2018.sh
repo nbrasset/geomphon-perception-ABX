17347a8eb5d44b3ef24d119257125b313fc93a9f
Amelia <post-doc@ogdoctd214.ogdoct.comp.univ-paris-diderot.fr>
2018-09-07 10:32:53 +0200

    updates output

--- experiments/pilot_Aug_2018/geomphon_Aug_2018.sh
+++ experiments/pilot_Aug_2018/geomphon_Aug_2018.sh
@@ -7,11 +7,9 @@
 #scale intensity of all files just created, save in stimuli/norm_intervals
 /Applications/Praat.app/Contents/MacOS/Praat --run "scale_intensity.Praat"
 
-#run filtering script to get right stim list 
-#this script saves stimlist to concatenation folder. 
+#run create_stimlist.py to create a stimlist 
+#this script saves stimlist in correct format  to concatenation folder. 
 #script also saves new sequence file based on this stimlst to LMEDs folders for English and french
-
-
 
 #use stimlist to run concatenation file on normalized stimuli 
 /Applications/Praat.app/Contents/MacOS/Praat --run "concatenation_of_tripets.Praat"
@@ -21,14 +19,7 @@ cd stimuli
 for i in *.wav ; do sox "$i" "$(basename -s .wav "$i").mp3"; done
 for i in *.wav ; do sox "$i" "$(basename -s .wav "$i").ogg"; done
 
-#move all the newly created .ogg and .mp3 files to where they need to be in the two separate LMEDs folders 
-cd ..
-
-cp stimuli/*.ogg LMEDS_master/tests/geomphon_ABX_English_Aug_2018/audio_and_video
-cp stimuli/*.mp3 LMEDS_master/tests/geomphon_ABX_English_Aug_2018/audio_and_video
-
-cp stimuli/*.ogg LMEDS_master/tests/geomphon_ABX_French_Aug_2018/audio_and_video
-cp stimuli/*.mp3 LMEDS_master/tests/geomphon_ABX_French_Aug_2018/audio_and_video
+#
 
 
 
