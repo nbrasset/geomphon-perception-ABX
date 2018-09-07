6e02800a7ab1904d09a7a451636a7257fca35830
ameliakimball <akimbal2@illinois.edu>
2018-09-07 12:46:17 +0200

    add script to reformat stimlist for concatenation

---
+++ experiments/pilot_Aug_2018/reformat_stimlist.R
@@ -0,0 +1,36 @@
+#this takes as input the output of the script "create_stimlist.py" and puts it
+#in the format needed for "concatenation_of_wavs.Praat"
+
+
+#Using plyr mapvalues, interpret numerals output by create_stimlist 
+#as meaningful strings 
+
+
+#split the comparison column into two columns, name them P and Q
+
+
+#add a Vowel or consonant column
+
+
+
+#Add the correct context, depending on whether a consonant or vowel
+
+#Add speaker  to P and Q based on value in input 
+
+#Create new columns "File1 "File2" "File3"
+
+# if loop fill in the file columns depending on the order column Ps and Qs
+
+#add in silence columns (identical for all stimuli)
+
+#create Column that has the correct answer 
+
+#create tripletid column (= just an index number, so this document can be
+#used as a key to determine the sounds during analysis.) 
+
+#print matrix to text file (NB NOT A CSV, Praat prefers TXT here.)  
+
+
+
+
+
