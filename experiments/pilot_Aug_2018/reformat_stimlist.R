ebdab70b04214bbec5eedb9fe499403228685a8d
ameliakimball <akimbal2@illinois.edu>
2018-09-09 16:18:05 +0200

    finsihes reformatting script

--- experiments/pilot_Aug_2018/reformat_stimlist.R
+++ experiments/pilot_Aug_2018/reformat_stimlist.R
@@ -50,30 +50,72 @@ list<- list %>% separate(COMPARISON, c("P", "Q"),sep="_")
 vowels <-c("HA","HAE","HEE","HOO","HUH","WHO")
 consonants<-c("P","T","K","F","TH","SH","SS")
 
-list %>% mutate(
+list<-list %>% mutate(
       V_C = case_when(
             P %in% vowels~"vowel",
             P %in% consonants ~"consonant"))
 
-
-
 #Add the correct context, depending on whether a consonant or vowel
+#add in a random repetition number to chose one of the repetitions of the word (random value 1-4)
+ 
+ list<-list %>% mutate(
+    word_P = case_when(
+      V_C == "vowel"& CONTEXT == "AH"~ paste(P,"DAH", sample(1:4,1), sep=""),
+      V_C == "vowel" & CONTEXT == "EE"~ paste(P,"DI",  sample(1:4,1),sep=""),
+      V_C == "consonant" & CONTEXT == "AH"~ paste("AH",P,sample(1:4,1),"AH", sep=""),
+      V_C == "consonant" & CONTEXT == "EE"~ paste("EE",P,sample(1:4,1),"EE", sep="")))
 
+ list<-list %>% mutate(
+    word_Q = case_when(
+      V_C == "vowel"& CONTEXT == "AH"~ paste(Q,"DAH",sample(1:4,1), sep=""),
+      V_C == "vowel" & CONTEXT == "EE"~ paste(Q,"DI",sample(1:4,1), sep=""),
+      V_C == "consonant" & CONTEXT == "AH"~ paste("AH",Q,"AH",sample(1:4,1), sep=""),
+      V_C == "consonant" & CONTEXT == "EE"~ paste("EE",Q,"EE",sample(1:4,1), sep=""))
+    )
+ 
+ 
 #Add speaker  to P and Q based on value in input 
+ list<-list %>% mutate(
+   FileP = paste(SPEAKER,word_P,sep=""),
+   FileQ = paste(SPEAKER,word_Q,sep=""))
+ 
+ 
+# create file and silence columns depending on the order column Ps and Qs
+ list<-list %>% mutate(
+   File1 = case_when(
+     ORDER == "PQP"~FileP,
+     ORDER == "PQQ"~ FileP,
+     ORDER == "QPQ"~ FileQ,
+     ORDER == "QPP"~ FileQ),
+   File2 = case_when(
+     ORDER == "PQP"~FileQ,
+     ORDER == "PQQ"~FileQ,
+     ORDER == "QPQ"~FileQ,
+     ORDER == "QPP"~FileP),
+   File3 = case_when(
+     ORDER == "QPQ"~FileQ,
+     ORDER == "PQQ"~FileQ,
+     ORDER == "PQP"~FileQ,
+     ORDER == "QPP"~FileP)
+   )
+ 
+ list<-list %>% mutate(
+ CORR_ANS = case_when(
+   File1==File3~"A",
+   File2 == File3 ~"B"))
 
-#Create new columns "File1 "File2" "File3"
+#add in columns with the name of the silences. 
+list$Silence1<-rep("500ms_silence.wav",length(list$File1))
+list$Silence2<-rep("500ms_silence.wav",length(list$File1))
 
-# if loop fill in the file columns depending on the order column Ps and Qs
+#create Column that has the filename for the concatenated file
+list$filename<-paste("stimulus",1:length(list$File1),sep="")
 
-#add in silence columns (identical for all stimuli)
+Stimuli_list<-list %>%
+  select(File1, Silence1,File2,Silence2,File3,CORR_ANS,filename)
 
-#create Column that has the correct answer 
-
-#create tripletid column (= just an index number, so this document can be
-#used as a key to determine the sounds during analysis.) 
-
-#print matrix to text file (NB NOT A CSV, Praat prefers TXT here.)  
-
+#print df  to text file (NB NOT A CSV, Praat prefers TXT here.)  
+write.table(Stimuli_list,file="Stimuli_list.txt", sep="\t")
 
 
 
