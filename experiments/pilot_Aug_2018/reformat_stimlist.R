1740fa36da4c325c295f11a58df40d3ecc4ec68c
ameliakimball <akimbal2@illinois.edu>
2018-10-04 16:38:04 +0200

    updates sampling of soundfiles in reformat_stimlist

--- experiments/pilot_Aug_2018/reformat_stimlist.R
+++ experiments/pilot_Aug_2018/reformat_stimlist.R
@@ -4,47 +4,43 @@
 library(plyr)
 library(dplyr)
 library(tidyr)
+set.seed(567)
 
-list<-read.csv(stimlist.csv)
-
-
-
+stimlist<-read.csv("stimlist.csv")
 #Using plyr mapvalues, interpret numerals output by create_stimlist 
 #as meaningful strings 
 
-
-#comparisons  #FIXME-add VC  info here?
-
-list$COMPARISON<-
-  mapvalues(list$COMPARISON,
+stimlist$COMPARISON<-
+  mapvalues(stimlist$COMPARISON,
             from = c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35),
             to=c("HA_HAE","HA_HEE","HA_HOO","HA_HUH","HA_WHO","HAE_HEE","HAE_HOO","HAE_HUH","HAE_WHO","HEE_HOO",
              "HEE_HUH","HEE_WHO","HOO_HUH","HOO_WHO","HOO_WHO","F_K","F_P","F_SH","F_SS","F_TH","F_T","K_P","K_SH",
              "K_SS","K_TH","K_T","P_SH","P_SS","P_TH","P_T","SH_SS","SH_TH","SH_T","SS_TH","SS_T","TH_T"),
             warn_missing = TRUE)
 
-list$ORDER<-
-  mapvalues(list$ORDER,
+stimlist$ORDER<-
+  mapvalues(stimlist$ORDER,
             from= c(0,1,2,3),
             to = c("PQP","QPP","PQQ","QPQ"),
             warn_missing = TRUE)
 
-list$SPEAKER<-
-  mapvalues(list$SPEAKER,
+stimlist$SPEAKER<-
+  mapvalues(stimlist$SPEAKER,
             from=c(0,1),
             to=c('ewan_','amelia_'),
             warn_missing = TRUE)
 
-list$CONTEXT<-
-  mapvalues(list$CONTEXT,
+stimlist$CONTEXT<-
+  mapvalues(stimlist$CONTEXT,
             from=c(0,1),
             to=c('EE','AH'),
             warn_missing = TRUE)
 
 
+
 #split the comparison column into two columns, name them P and Q
-list<-as.data.frame(list)
-list<- list %>% separate(COMPARISON, c("P", "Q"),sep="_")
+stimlist<-as.data.frame(stimlist)
+stimlist<- stimlist %>% separate(COMPARISON, c("P", "Q"),sep="_")
 
 
 #add a Vowel or consonant column
@@ -52,7 +48,7 @@ list<- list %>% separate(COMPARISON, c("P", "Q"),sep="_")
 vowels <-c("HA","HAE","HEE","HOO","HUH","WHO")
 consonants<-c("P","T","K","F","TH","SH","SS")
 
-list<-list %>% mutate(
+stimlist<-stimlist %>% mutate(
       V_C = case_when(
             P %in% vowels~"vowel",
             P %in% consonants ~"consonant"))
@@ -60,24 +56,41 @@ list<-list %>% mutate(
 #Add the correct context, depending on whether a consonant or vowel
 #add in a random repetition number to chose one of the repetitions of the word (random value 1-4)
  
- list<-list %>% mutate(
+stimlist<-stimlist %>% mutate(
     word_P = case_when(
-      V_C == "vowel"& CONTEXT == "AH"~ paste(P,"DAH",sample(1:4,1),".wav", sep=""),
-      V_C == "vowel" & CONTEXT == "EE"~ paste(P,"DI",sample(1:4,1),".wav", sep=""),
-      V_C == "consonant" & CONTEXT == "AH"~ paste("A",P,"AH",sample(1:4,1), ".wav",sep=""),
-      V_C == "consonant" & CONTEXT == "EE"~ paste("EE",P,"EE",sample(1:4,1), ".wav",sep="")))
+      V_C == "vowel"& CONTEXT == "AH"~ paste(P,"DAH", sep=""),
+      V_C == "vowel" & CONTEXT == "EE"~ paste(P,"DI", sep=""),
+      V_C == "consonant" & CONTEXT == "AH"~ paste("A",P,"AH",sep=""),
+      V_C == "consonant" & CONTEXT == "EE"~ paste("EE",P,"EE",sep="")))
 
- list<-list %>% mutate(
+stimlist<-stimlist %>% mutate(
     word_Q = case_when(
-      V_C == "vowel"& CONTEXT == "AH"~ paste(Q,"DAH",sample(1:4,1),".wav", sep=""),
-      V_C == "vowel" & CONTEXT == "EE"~ paste(Q,"DI",sample(1:4,1), ".wav",sep=""),
-      V_C == "consonant" & CONTEXT == "AH"~ paste("A",Q,"AH",sample(1:4,1), ".wav",sep=""),
-      V_C == "consonant" & CONTEXT == "EE"~ paste("EE",Q,"EE",sample(1:4,1), ".wav",sep=""))
+      V_C == "vowel"& CONTEXT == "AH"~ paste(Q,"DAH", sep=""),
+      V_C == "vowel" & CONTEXT == "EE"~ paste(Q,"DI",sep=""),
+      V_C == "consonant" & CONTEXT == "AH"~ paste("A",Q,"AH",sep=""),
+      V_C == "consonant" & CONTEXT == "EE"~ paste("EE",Q,"EE",sep=""))
     )
  
 
+#map value of word_P and word_Q to an acutal file name 
+#first create vector of all file names intervals 
+files_list<-dir(path ="stimuli/concatenation/intervals")
+
+#add column for speaker by stripping off speaker name 
+
+#add column for sound by stripping off number and .csv
+
+
+#group by speaker 
+#then group by 
+#then sample one in each group and return one value of file name. 
+
+
+
+
+
 # create file and silence columns depending on the order column Ps and Qs
- list<-list %>% mutate(
+stimlist<-stimlist %>% mutate(
    File1 = case_when(
      ORDER == "PQP"~word_P,
      ORDER == "PQQ"~ word_P,
@@ -96,35 +109,35 @@ list<-list %>% mutate(
    )
  
  #add correct answer-- nb do this before adding speaker because speaker will be different across these.  
- list<-list %>% mutate(
+stimlist<-stimlist %>% mutate(
  CORR_ANS = case_when(
-   list$File1==list$File3~"A",
-   list$File2 == list$File3 ~"B"))
+   stimlist$File1==stimlist$File3~"A",
+   stimlist$File2 ==stimlist$File3 ~"B"))
 
  
  #Add speaker  to files-- one speaker for files 1 and 2, the other for file 3 
- list<-list %>% mutate(
+stimlist<-stimlist %>% mutate(
    File1 = paste(SPEAKER,File1,sep=""),
    File2 = paste(SPEAKER,File2,sep=""))
 
- list<-list %>% mutate(
+stimlist<-stimlist %>% mutate(
    File3 = case_when(
      SPEAKER=="amelia_"~paste("ewan_",File3,sep=""),
      SPEAKER=="ewan_"~paste("amelia_",File3,sep="")))
 
  
 #add in columns with the name of the silences. 
-list$Silence1<-rep("500ms_silence.wav",length(list$File1))
-list$Silence2<-rep("500ms_silence.wav",length(list$File1))
+stimlist$Silence1<-rep("500ms_silence.wav",length(stimlist$File1))
+stimlist$Silence2<-rep("500ms_silence.wav",length(stimlist$File1))
 
 #create Column that has the filename for the concatenated file
-list$filename<-paste("stimulus",1:length(list$File1),sep="")
+stimlist$filename<-paste("stimulus",1:length(stimlist$File1),sep="")
 
-Stimuli_list<-list %>%
+final_stimlist<-stimlist %>%
   select(File1,Silence1,File2,Silence2,File3,CORR_ANS,filename)
 
 #print df  to text file (NB NOT A CSV, Praat prefers TXT here.)  
-write.table(Stimuli_list, file="Stimuli_list.txt", sep="\t",quote = FALSE, row.names = FALSE)
+write.table(final_stimlist, file="Stimuli_list.txt", sep="\t",quote = FALSE, row.names = FALSE)
 
 
 
