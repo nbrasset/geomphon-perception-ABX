b96cbd7232991c405418568c746eda51e15f2181
ameliakimball <akimbal2@illinois.edu>
2019-05-20 09:02:28 +0200

    changes loo comparison to use only models that have finished running

--- model_scripts/compare_loos.R
+++ model_scripts/compare_loos.R
@@ -11,16 +11,53 @@ wrong_model<-wrong_mods$model_data_name
 mod_pairs<-cbind(wrong_model,correct_model)
 mod_pairs<-as.data.frame(mod_pairs)
 
-#compare loos of those pairs, return output to new file 
+
+#get subset of pairs in mod pairs where both files exist
+rds_list<- list.files("~/model_scripts/model_scripts/hindi_kab_rds")
+cor_list<-list()
+wrong_list<-list()
+
+for (i in 1:nrow(mod_pairs)){
+  if ((paste0(mod_pairs$wrong_model[i],".rds") %in% rds_list) &
+      (paste0(mod_pairs$correct_model[i],".rds") %in% rds_list)) {
+          cor_list[i]<-paste(mod_pairs$correct_model[i])
+          wrong_list[i]<-paste(mod_pairs$wrong_model[i])
+  }
+}
+
+output_pairs<- cbind(cor_list,wrong_list)
+output_pairs<-as.data.frame(output_pairs)
+
+done_pairs<-dplyr::filter(output_pairs, output_pairs$cor_list!="NULL")
+done_pairs$wrong_model<-done_pairs$wrong_list
+done_pairs$correct_model<-done_pairs$cor_list
 
 
-for i in (1:nrow(mod_pairs)) {
-  wrong_mod<-readRDS("econ_1_loc_-1_glob_-1_mod_loc.rds")
-  corr_mod<-readRDS(mod_pairs$correct_model[i])
+
+#calculate loos for remaining pairs. 
+rds_folder<-"hindi_kab_rds"
+
+correct_model<-list()
+wrong_model<-list() 
+comparison<-list()
+
+
+for (i in 11:15) {
+  wrong_mod<-readRDS(paste0(rds_folder,"/",done_pairs$wrong_model[i],".rds"))
+  corr_mod<-readRDS(paste0(rds_folder,"/",done_pairs$correct_model[i],".rds"))
   loo_wrong<-loo(wrong_mod)
   loo_corr<- loo(corr_mod)
-  comparison<-compare(loo_wrong,loo_corr)
-  print(mod_pairs$wrong_model[i],mod_pairs$correct_model[i], comparison)
+  comp<-compare(loo_wrong,loo_corr)
+  
+  correct_model[i]<-paste0(rds_folder,"/",done_pairs$correct_model[i],".rds")
+  wrong_model[i]<-paste0(rds_folder,"/",done_pairs$wrong_model[i],".rds")
+  comparison[[i]]<-comp
 }
+
+
+comp_df<-as.data.frame(cbind(correct_model,wrong_model,comparison))
+
+
+
 
 
